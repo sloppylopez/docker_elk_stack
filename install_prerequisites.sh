0b2fcca2408718f3e6dc4b8f23c287ef3d5308a8
sloppylopez <18N3m3s1s>
2015-10-18 19:30:09 +0200

    :new: docker-compose travis integration

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -8,6 +8,13 @@ wget -qO- https://get.docker.com/ | sh
 #For not having to do 'sudo docker' but this represents a security issue so i am leaving it unset by default
 #sudo usermod -aG docker <YOUR_UBUNTU_USER>
 
+#Install travis-cli command line to be able to encrypt api_keys in the Travis build
+sudo gem install travis
+#Encrypt your code climate key for Travis build and add it automatically to your .travis.yml
+#remember explicitly declare this variables in
+#https://travis-ci.org/<YOUR_GIT_USER>/<YOUR_GIT_PROJECT_NAME>/settings/env_vars
+travis encrypt DOCKER_USER=<YOUR_DOCKER_USER> --add && travis encrypt DOCKER_PASSWORD=<YOUR_DOCKER_PASSWORD> --add && travis encrypt DOCKER_EMAIL=<YOUR_DOCKER_EMAIL> --add
+
 #Docker-Compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.3.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
--- install_prerequisites_mac.sh
+++ install_prerequisites_mac.sh
@@ -2,6 +2,13 @@
 #Pre-requisites
 brew install curl docker docker-compose
 
+#Install travis-cli command line to be able to encrypt api_keys in the Travis build
+sudo gem install travis
+#Encrypt your code climate key for Travis build and add it automatically to your .travis.yml
+#remember explicitly declare this variables in
+#https://travis-ci.org/<YOUR_GIT_USER>/<YOUR_GIT_PROJECT_NAME>/settings/env_vars
+travis encrypt FIREBASE_TOKEN=<YOUR_FIREBASE_TOKEN> --add
+
 #Check installed versions
 docker --version
 docker-compose --version
